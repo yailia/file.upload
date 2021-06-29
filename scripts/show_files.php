27df018fac4c2ac69741e970b44fae6ef6f3af38
Ivanov I <ilia@ivanov.press>
2021-06-29 17:45:15 +0300

    FIX: вывод в гвлерею, ошибки;

--- scripts/show_files.php
+++ scripts/show_files.php
@@ -6,12 +6,16 @@ function makeImagesList ($arr)
     echo 'Нет загруженных картинок';
 }
     for ($i = 2; $i < count($arr); $i++) {
+      $picturePath = '/upload' . '/' . $arr[$i];
      echo "<li style='border:1px solid gray; width:30vw; padding:1rem; list-style:none;'>
+     <img style='max-height:100px;' src=$picturePath >
      <label style='display:flex'>
         $arr[$i] <br />
         <input method='POST' style='margin-left:auto' type='checkbox' name='delete[$arr[$i]]' id=''>
       </label>
     </li>";
     }
+
+    var_dump($arr);
 };
  
